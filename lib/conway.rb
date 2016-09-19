6eb584f697bde6200933eb64ac4fb4133965161e
nlkassad <nlkassad@hotmail.com>
2016-09-19 15:28:58 -0400

    Add Attributes for Cell and World

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,5 +1,14 @@
 class World
+  def initialize (width, height)
+    @width = width
+    @height = height
+    @cells = []
+  end
 end
 
 class Cell
+  def initialize
+    @position = 0
+    @life_state = false
+  end
 end
