7fb12d9d049ac48c2555012f0efe28a03b2b4e66
nlkassad <nlkassad@hotmail.com>
2016-09-19 13:34:07 -0400

    Add some Ruby Code

--- lib/conway.rb
+++ lib/conway.rb
@@ -5,15 +5,30 @@
 #   life status of cell
 #   neighbors of the cell
 class Cell
-  def initialize (position:, life_status:, neighbors:)
+  attr_writer :life_status
+  def initialize (position, neighbors)
+    @position = position
+    @neighbors = neighbors
+    @life_status = "dead"
+  end
 end
 #world
 
-world
-  size of the world
-  the cells in the world
-  state of the world (paused or active)
-  iteration of the world
+# world
+#   size of the world
+#   the cells in the world
+#   state of the world (paused or active)
+#   iteration of the world
+
+class World
+  attr_reader :cells
+  def initialize (size)
+    @size = size
+    @world_state = "paused"
+    @iteration = 0
+    @cells = []
+  end
+end
 
 change world state
 iterate over world
