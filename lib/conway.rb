81f2a0bcc47a38f4cbc4dd6ca0a88a0059c7db5a
nlkassad <nlkassad@hotmail.com>
2016-09-19 13:21:07 -0400

    Create Game Version

---
+++ lib/conway.rb
@@ -0,0 +1,29 @@
+#cell
+
+cell
+  position of the cell
+  life status of cell
+  neighbors of the cell
+
+#world
+
+world
+  size of the world
+  the cells in the world
+  state of the world (paused or active)
+  iteration of the world
+
+change world state
+iterate over world
+
+iterate
+  
+
+# over population
+  # more than three live neighbors
+# under population
+  # less than 2 live neighbors
+# birth
+  # dead cell with three live neighbors
+# static
+  # cell with two or three live neighbors
